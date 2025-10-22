94d3ef67246ac1bcf2aec0c9b6feb888dbce1232
HollowAOSP <ahmedshahinx0x@gmail.com>
2025-10-22 18:34:31 -0400

    penangf: Import audio configs from UHAS34.29-20
    Change-Id: I38f55c1e3a53e6ec404c32845085a3210fc6a9bb

--- device.mk
+++ device.mk
@@ -7,5 +7,10 @@
 # Inherit from mt6768-common
 $(call inherit-product, device/motorola/mt6768-common/mt6768.mk)
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/audio_device_dm_aw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device_dm_aw.xml \
+    $(LOCAL_PATH)/configs/audio/audio_device_sm_aw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_device_sm_aw.xml
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/motorola/penangf/penangf-vendor.mk)
